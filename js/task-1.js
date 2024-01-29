110a5aa1a0ba895dedc15a2322f6cc90eb77f547
klimisnotavailable <klimgrigor28@gmail.com>
2024-01-30 00:18:04 +0200

    tech

--- js/task-1.js
+++ js/task-1.js
@@ -5,8 +5,6 @@ console.log(`Number of categories: ${list.children.length}`);
 
 const listItems = document.querySelectorAll('li.item');
 
-const technologies = document.querySelectorAll('li.item > ul > li')
-
 listItems.forEach(item => {
     console.log(`Category: ${item.firstElementChild.textContent}`)
     console.log(`Elements: ${item.querySelectorAll('ul > li').length}`)
